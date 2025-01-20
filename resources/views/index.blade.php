f883ff0ba0a70e68b65db46b8302223b11f4ad01
Andrew <code70@inbox.ru>
2025-01-20 21:16:19 +0700

    rebuild index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,14 @@
-Hello world!
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Тепло на белой</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+</head>
+<body>
+    <h2>Тепло на белой</h2>
+</body>
+</html>
