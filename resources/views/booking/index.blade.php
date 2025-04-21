7b2100a8ecb4d5a5b30e8ba50cba07b792001559
Andrew Master <code70@inbox.ru>
2025-04-21 21:17:47 +0700

    add meta for csrf

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     @vite('resources/js/app.js')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
